20b9247a66c5b9f73466634fb5c71215849c5274
henry <jaohaohsuan@gmail.com>
2016-10-22 10:41:16 +0800

    use input ipxe script basename as out put iso file

--- ipxe/run.sh
+++ ipxe/run.sh
@@ -5,4 +5,4 @@ cp /scripts/*.ipxe /ipxe/src/
 cd /ipxe/src
 make bin/ipxe.iso
 make bin/ipxe.iso EMBED="$1" 
-exec cp -v bin/ipxe.iso /scripts/iso/
+exec cp -v bin/ipxe.iso /scripts/iso/$(basename -s .ipxe $1).iso
